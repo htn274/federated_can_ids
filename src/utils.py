94e9d5a3e1896f259b9c2295d3f9e4edea5c9b8c
htn274 <thiennust274@gmail.com>
2022-11-01 17:08:42 +0900

    add FedAvg FN

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,12 @@
 from typing import OrderedDict
-from sklearn.metrics import confusion_matrix
+from anyio import Path
+from sklearn.metrics import confusion_matrix, f1_score
 import pandas as pd
 import numpy as np
 import torch
+from torch.utils.data import DataLoader
+import pytorch_lightning as pl
+from dataset import CANDataset
 
 def get_parameters(model):
     """
@@ -39,3 +43,17 @@ def print_results(results, classes):
         res = [round(results[k][idx], 4) for k in results.keys()]
         output = [c] + res
         print('\t'.join(map(str, output)))
+
+def test_model(data_dir, model):
+    transform = None
+    test_dataset = CANDataset(root_dir=Path(data_dir)/'test', is_binary=True, transform=transform)
+    test_loader = DataLoader(test_dataset, batch_size=512, shuffle=False, 
+                        pin_memory=True, sampler=None)
+    trainer = pl.Trainer(enable_checkpointing=False, logger=False)
+    results = trainer.predict(model, dataloaders=test_loader)
+    labels = np.concatenate([x['labels'] for x in results])
+    preds = np.concatenate([x['preds'] for x in results])
+    tn, fp, fn, tp = confusion_matrix(labels, preds).ravel()
+    f1 = f1_score(labels, preds)
+    err = (fn + fp) / len(preds)
+    return (f1, err)
